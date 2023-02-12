48c1a081cdaea74d9932952df2f6a6a7de40b648
Susam Pal <susam@susam.net>
2023-02-12 16:09:20 +0000

    Mark main function in Python for internal use only

--- src/pcface.py
+++ src/pcface.py
@@ -264,7 +264,7 @@ given font.
     return args
 
 
-def main():
+def _main():
     """Generate previews, graphs, and bitmap arrays."""
     args = _parse_arguments()
     chars = cp437chars()
@@ -327,4 +327,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    _main()
